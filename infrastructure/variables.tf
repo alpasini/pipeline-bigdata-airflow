7cbd8376142a7ea30e450449e5978b5a68ffafb5
Alex Pasini <alpasini@gmail.com>
2022-03-17 07:25:02 -0300

    Subindo código

---
+++ infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
+}
+
+variable "key_pair_name" {
+  default = "pasini-igti-teste" ## Criar uma na página do EC2
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-18c98a54" ##pegar o numero em VPC na AWS
+}
+
+variable "vpc_id" {
+  default = "vpc-c2dd58a9" ##pegar o numero em VPC na AWS
+}
