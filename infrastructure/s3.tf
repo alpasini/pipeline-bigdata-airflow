75f158aff2ec0d68b3581bd527887b30b82f20fe
Alex Pasini <alpasini@gmail.com>
2022-03-17 07:32:03 -0300

    Corrigindo S#

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,19 +1,23 @@
+# HCL - Hashicorp Configuration Language
+# Linguagem declarativa
+
 resource "aws_s3_bucket" "dl" {
+  #Parametros de configuração do recurso escolhido
   bucket = "datalake-pasini-tf"
-  acl    = "private"
 
   tags = {
-    IES   = "IGTI",
+    IES   = "IGTI"
     CURSO = "EDC"
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
   }
 }
 
+# Criptografia em Rest utilizando algorítimo default do S3 (AES256)
+resource "aws_s3_bucket_server_side_encryption_configuration" "dl" {
+  bucket = aws_s3_bucket.dl.id
 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
